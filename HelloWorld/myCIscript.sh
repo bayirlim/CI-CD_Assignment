447ab2ebe047172bdb9caceafebaef656070e92e
Merve Bayirli <merve_bayirli@hotmail.de>
2020-07-26 19:13:32 +0200

    Initial version of Hello Kubernetes

---
+++ HelloWorld/myCIscript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+DOCKERFILE=Dockerfile
+TAG="helloworld"
+USERNAME="merveba"
+NAME="hello-world"
+YAML="hello-application.yaml"
+
+docker build -t $USERNAME/$TAG .
+docker push $USERNAME/$TAG
+kubectl apply -f $YAML
+#kubectl create deployment hello-node --image=k8s.gcr.io/echoserver:1.4 ------- is already in YAML
+kubectl expose deployment hello-world  $NAME --type=LoadBalancer --port=8080
