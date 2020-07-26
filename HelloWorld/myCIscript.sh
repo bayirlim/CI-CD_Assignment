acc92ad9b23e0aba752c5bfb9a9dca6339840335
Merve Bayirli <merve_bayirli@hotmail.de>
2020-07-26 21:07:15 +0200

    Expose from script is now integrated as a service in the application.

--- HelloWorld/myCIscript.sh
+++ HelloWorld/myCIscript.sh
@@ -9,4 +9,4 @@ docker build -t $USERNAME/$TAG .
 docker push $USERNAME/$TAG
 kubectl apply -f $YAML
 #kubectl create deployment hello-node --image=k8s.gcr.io/echoserver:1.4 ------- is already in YAML
-kubectl expose deployment hello-world  $NAME --type=LoadBalancer --port=8080
+# kubectl expose deployment $NAME --type=LoadBalancer --port=8080
